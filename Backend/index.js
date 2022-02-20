66b0e5c8f5b7369c097edab56577586846aa07d0
Intezar Ahmad <31982418+theintezar@users.noreply.github.com>
2022-02-21 02:52:42 +0530

    bac

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const helmet  = require('helmet');
+const mongoose  = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require("dotenv");
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
+
+const app = express();
+
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL, ()=>{
+    console.log("database connected")
+});
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//apis
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
+
+
+app.listen(1006, ()=>{
+    console.log("1006 running")
+})
+//yo0eZhKIiyYSH6LM
